474b5cde9dc399426cd9dd4d54073b33bed74932
Dr. Korpusikpatelmia1 <patelmia24@gmail.com>
2026-04-13 18:58:02 -0700

    mias is valid date

---
+++ is_valid_date.py
@@ -0,0 +1,9 @@
+from days_in_month import days_in_month
+
+def is_valid_date(year, month, day):
+    if month not in range(1, 13):
+        return False
+    if day < 1:
+        return False
+    return day <= days_in_month(year, month)from days_in_month import days_in_month
+#version 2
